d0f1cb7e6567736dc861c07321d8faade815a4fe
courtnaymcewen <courtnaymcewen@gmail.com>
2019-07-29 20:33:34 -0500

    first commit

--- apps/semi_restful_tvshow_app/models.py
+++ apps/semi_restful_tvshow_app/models.py
@@ -1,7 +1,20 @@
-
+from __future__ import unicode_literals
 from django.db import models
 
-    
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {
+        }           # add keys and values to errors dictionary for each invalid field
+        if len(postData['title']) < 1:
+            errors["title"] = "Show title name should be at least 1 character"
+        if len(postData['network']) < 1:
+            errors["network"] = "Show network name should be at least 2 characters"
+        if len(postData['release_date']) < 10:
+            errors["release_date"] = "Show date name should be at least 10 characters"
+        if len(postData['description']) < 2:
+            errors["description"] = "Show date name should be at least 2 characters"
+        return errors    
+        
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.TextField()
@@ -9,6 +22,6 @@ class Show(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects=ShowManager()
     
-    # def __repr__(self): 
-    #     return f"Show: {self.id} {self.title} ({self.description})"
+
--- apps/semi_restful_tvshow_app/views.py
+++ apps/semi_restful_tvshow_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.contrib import messages
 
 def all_shows(request):
     a_shows = Show.objects.all()
@@ -12,7 +13,7 @@ def new_show(request):#shows the form
     print ('new_tvshow')
     return render(request, "semi_restful_tvshow_app/new_show.html")
 
-def edit_show(request, show_id):
+def edit_show(request, show_id): 
     show = Show.objects.get(id=show_id)
     context = {
         'show' : show
@@ -27,25 +28,41 @@ def show_detail(request, show_id):
     return render(request, "semi_restful_tvshow_app/show_detail.html", context)
 
 def create_show(request): #processing behind the scenes adds info to database
+    errors=Show.objects.basic_validator(request.POST) 
     print ('new_tvshow')
     print (f'request:{request}')
-    Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], description=request.POST['description'])
-    show = Show.objects.last()
-    show.save()
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        return redirect('/show/new')
+    else:    
+        Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], description=request.POST['description'])
+        show = Show.objects.last()
+        show.save()
     return redirect(f'/show/{show.id}')
 
-def update_show(request, show_id): #processing behind the scenes adds info to database
-    show = Show.objects.get(id=show_id)
-    print ("*************************************", request.POST)
-    # print (f'{request.POST["title"]}')
-    show.title = request.POST['title']
-    show.network = request.POST['network']
-    show.description = request.POST['description']
-    show.release_date = request.POST['release_date']
-    show.save()
-    context = {
-        'show' : show
-    }
+def update_show(request, show_id):#processing behind the scenes adds info to database
+    errors=Show.objects.basic_validator(request.POST) 
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        return redirect(f'/show/{show_id}/edit')
+    else:    
+        show = Show.objects.get(id=show_id)
+        print ("*************************************", request.POST)
+        # print (f'{request.POST["title"]}')
+        show.title = request.POST['title']
+        show.network = request.POST['network']
+        show.description = request.POST['description']
+        show.release_date = request.POST['release_date']
+        show.save()
+        context = {
+            'show' : show
+        }
     return redirect(f'/show/{show.id}')
 
 def destroy_show(request, show_id):
